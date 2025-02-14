9eb31a8baa04ade9c4d1eef46357fcc989654c14
Sarthak Srivastava <srivastavasarthak001@gmail.com>
2025-02-14 21:35:50 +0530

    Added functionality into stats

--- stats.c
+++ stats.c
@@ -2,14 +2,14 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
+ * @file <Add File Name>
  * @brief <Add Brief Description Here >
  *
  * <Add Extended Description Here>
@@ -19,29 +19,108 @@
  *
  */
 
-
-
 #include <stdio.h>
 #include "stats.h"
 
 /* Size of the Data Set */
 #define SIZE (40)
 
-void main() {
+int find_mean(int arr[], int len)
+{
+    int sum = 0;
+    for (int i = 0; i < len; i++)
+    {
+        sum += arr[i];
+    }
+    return sum / len;
+}
 
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
+int find_median(int arr[], int len)
+{
+    if (len % 2 == 0)
+    {
+        return (arr[len / 2] + arr[len / 2 - 1]) / 2;
+    }
+    else
+    {
+        return arr[len / 2];
+    }
+}
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+int find_maximum(int arr[], int len)
+{
+    int max = arr[0];
+    for (int i = 1; i < len; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
 
-  int len = sizeof(test) / sizeof(test[0]);
+int find_minimum(int arr[], int len)
+{
+    int min = arr[0];
+    for (int i = 1; i < len; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
 
-  
+int *sort_array(int arr[], int len)
+{
+    int temp;
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = i + 1; j < len; j++)
+        {
+            if (arr[i] < arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
 
+void print_statistics(int max, int min, int mean, int median)
+{
+    printf("Maximum: %d\n", max);
+    printf("Minimum: %d\n", min);
+    printf("Mean: %d\n", mean);
+    printf("Median: %d\n", median);
+}
+
+void main()
+{
+
+    unsigned char test[SIZE] = {34, 201, 190, 154, 8, 194, 2, 6,
+                                114, 88, 45, 76, 123, 87, 25, 23,
+                                200, 122, 150, 90, 92, 87, 177, 244,
+                                201, 6, 12, 60, 8, 2, 5, 67,
+                                7, 87, 250, 230, 99, 3, 100, 90};
+
+    /* Other Variable Declarations Go Here */
+    /* Statistics and Printing Functions Go Here */
+
+    int len = sizeof(test) / sizeof(test[0]);
+
+    int mean = find_mean(test, len);
+    int max = find_maximum(test, len);
+    int min = find_minimum(test, len);
+
+    int *sorted = sort_array(test, len);
+    int med = find_median(sorted, len);
+
+    print_statistics(max, min, mean, med);
 }
 
 /* Add other Implementation File Code Here */
--- stats.h
+++ stats.h
@@ -43,17 +43,82 @@
 #endif /* __STATS_H__ */
 
 
-int print_statistics(int max, int min, int mean, int median);
+/**
+ * @brief Prints the statistics of the data set.
+ *
+ * This function takes the maximum, minimum, mean, and median values
+ * and prints them to the standard output.
+ *
+ * @param max The maximum value in the data set.
+ * @param min The minimum value in the data set.
+ * @param mean The mean (average) value of the data set.
+ * @param median The median value of the data set.
+ */
+void print_statistics(int max, int min, int mean, int median);
 
-
+/**
+ * @brief Prints the elements of the array.
+ *
+ * This function takes an array and its length as input and prints
+ * each element of the array to the standard output.
+ *
+ * @param arr The array of integers to be printed.
+ * @param len The length of the array.
+ * @return The number of elements printed.
+ */
 int print_array(int arr[], int len);
 
+/**
+ * @brief Finds the median of the data set.
+ *
+ * This function calculates the median value of the given array.
+ *
+ * @param arr The array of integers.
+ * @param len The length of the array.
+ * @return The median value of the array.
+ */
 int find_median(int arr[], int len);
 
+/**
+ * @brief Finds the mean of the data set.
+ *
+ * This function calculates the mean (average) value of the given array.
+ *
+ * @param arr The array of integers.
+ * @param len The length of the array.
+ * @return The mean value of the array.
+ */
 int find_mean(int arr[], int len);
 
+/**
+ * @brief Finds the maximum value in the data set.
+ *
+ * This function searches for the maximum value in the given array.
+ *
+ * @param arr The array of integers.
+ * @param len The length of the array.
+ * @return The maximum value in the array.
+ */
 int find_maximum(int arr[], int len);
 
+/**
+ * @brief Finds the minimum value in the data set.
+ *
+ * This function searches for the minimum value in the given array.
+ *
+ * @param arr The array of integers.
+ * @param len The length of the array.
+ * @return The minimum value in the array.
+ */
 int find_minimum(int arr[], int len);
 
-int sort_array(int arr[], int len);
+/**
+ * @brief Sorts the array in ascending order.
+ *
+ * This function sorts the given array and returns a pointer to the sorted array.
+ *
+ * @param arr The array of integers to be sorted.
+ * @param len The length of the array.
+ * @return A pointer to the sorted array.
+ */
+int* sort_array(int arr[], int len);
